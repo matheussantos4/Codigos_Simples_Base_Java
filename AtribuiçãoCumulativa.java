973370740553cd4a50218dbe78c8d78ba0283a00
matheussantos4 <matheus.santos88y@gmail.com>
2026-03-17 18:05:41 -0300

    atribuiçãoCumulativa

---
+++ AtribuiçãoCumulativa.java
@@ -0,0 +1,22 @@
+public class AtribuiçãoCumulativa {
+
+    // Código simples com atribuição cumulativa.
+
+    public static void main(String[] args) {
+
+        int saldo = 1000;
+
+        saldo += 1500; // Recebeu salário
+        System.out.println("Após salário: " + saldo);
+        saldo -= 800; // Pagou aluguel
+        System.out.println("Após aluguel: " + saldo);
+        saldo -= 300; // pagou contas
+        System.out.println("Após contas: " + saldo);
+        saldo *= 0.8; // investiu uma parte
+        System.out.println("Após investir: " + saldo);
+        saldo /= 2; // após guardar metade
+        System.out.println("Após guardar: " + saldo);
+
+    }
+
+}
